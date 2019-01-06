740cc163ae998809f772b33021bd4d60fed3fb98
Max Manuel <maxcamina@gmail.com>
2019-01-06 07:35:17 -0500

    added database access

--- config/database.php
+++ config/database.php
@@ -1,9 +1,33 @@
 <?php
-$url = parse_url(getenv('DATABASE_URL'));
-$host = $url["host"]?? null;
-$username = $url["user"]?? null;
-$password = $url["pass"]?? null;
-$database = substr($url["path"],1)?? null;
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+    array(
+        'pdo.server' => array(
+            'driver'   => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"],'/')
+        )
+    )
+);
+
+$app->get('/db/', function() use($app) {
+    $st = $app['pdo']->prepare('SELECT name FROM test_table');
+    $st->execute();
+
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
+
+    return $app['twig']->render('database.twig', array(
+        'names' => $names
+    ));
+});
+
 
 return [
 
